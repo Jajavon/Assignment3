05bf9ff8deec30f976a96483003b59c6e7a6e5e2
Jonathan Ajavon <jajavon@uci.edu>
2024-03-01 21:13:47 -0800

    write all the files where are the information and data is stored for the search function to use

--- search.py
+++ search.py
@@ -4,6 +4,11 @@
 #   At least the following queries should be used to test your retrieval:
 #   cristina lopes, machine learning, ACM, master of software engineering
 
+DOC_IDS_FILE = "./docsIDs.txt"
+INV_INDEX_FILE = "./InvIndex_dict.txt"
+TERMS_IDS_FILE = "./termsIDs.txt"
+DEV_FILE = "./ANALYST/"
+
 def main():
     {}
 
