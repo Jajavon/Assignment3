8e3a1d1554579f35b59eaf71e1c0e77192d2b735
Jonathan Ajavon <jajavon@uci.edu>
2024-03-01 21:19:40 -0800

    Created scanInvIndex which opens & reads each line of the invertedIndex textfile and then save it into the variable to be used for search

--- search.py
+++ search.py
@@ -11,6 +11,26 @@ DEV_FILE = "./ANALYST/"
 
 def search(text):
     return input(text)
+def scanInvIndex():
+    invIndex = dict()
+
+    with open(INV_INDEX_FILE, "r") as indexFile:
+        lines = set(indexFile.readlines())
+        for l in lines:
+            l= l.replace('\n', '')
+            data = l.split(" ")
+            doc_id = int(float(data[0].strip()))
+            words = set(data[2:])
+            invIndex[doc_id] = {}
+
+            for w in words:
+                t = w.strip()
+                if t in invIndex[doc_id]:
+                    invIndex[doc_id][t] += 1
+                else:
+                    invIndex[doc_id][t] = 1
+
+    return invIndex;
 def main():
     {}
 
