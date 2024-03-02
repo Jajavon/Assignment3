c887ee0382c3b72e33427dca7fd6b02768cfcaac
Jonathan Ajavon <jajavon@uci.edu>
2024-03-01 19:48:20 -0800

    Created main function which will be where all of the code of search will take place

---
+++ search.py
@@ -0,0 +1,11 @@
+# Searcher
+
+#   search and retrieval component
+#   At least the following queries should be used to test your retrieval:
+#   cristina lopes, machine learning, ACM, master of software engineering
+
+def main():
+    {}
+
+if __name__ == "__main__":
+    main()
